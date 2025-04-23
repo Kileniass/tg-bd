a262ba6c48526c705b140e5d4bd88903dd74a340
Mihail Dernov <Googledinoru@mail.ru>
2025-04-23 23:12:18 +0700

    FIX

--- app/schemas.py
+++ app/schemas.py
@@ -19,7 +19,7 @@ class UserRead(UserBase):
     telegram_id: str
 
     class Config:
-        orm_mode = True
+        from_attributes = True
 
 class LikeCreate(BaseModel):
     from_user_id: int
@@ -31,7 +31,7 @@ class MatchRead(BaseModel):
     user2_id: int
 
     class Config:
-        orm_mode = True
+        from_attributes = True
 
 class AboutUpdate(BaseModel):
     user_id: int
